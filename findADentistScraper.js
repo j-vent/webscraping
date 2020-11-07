e63ee0d5ed78a0dcdfdbe5b225e17d6d36364786
jvent <justine.ventura14@gmail.com>
2020-11-07 12:04:45 -0700

    basic scraping for one zipcode

--- findADentistScraper.js
+++ findADentistScraper.js
@@ -1,34 +1,31 @@
 const input = require('./zipcodes.json');
+// loop later and place into url 
 console.log(input.zipcodes[0]);
+
 const rp = require('request-promise');
 const cheerio = require('cheerio');
 
-// const zipcodes = JSON.parse(input);
 
-// for (z in zipcodes) {
-//     console.log(zipcodes[z]);
-// }
-
-
-const url = 'https://findadentist.ada.org/search-results?specialty=1&address=30304&searchResultsReferrer=true';
+const url = 'https://findadentist.ada.org/search-results?&address=' + 30301;
 
 rp(url)
     .then(function (html) {
         const $ = cheerio.load(html);
+        // console.log(html);
         console.log("test");
 
-        const names = $(".dentist-list__item__content").find('a').first().text();
+        const names = $('.dentist-list__item__content').find('a').text();
         console.log(names);
 
         const specialities = $('.specialty').text();
         console.log(specialities);
 
-        const phoneNums = $('.phone').text();
-        console.log(phoneNums);
+        // const phoneNums = $('.phone').text();
+        // console.log("phones " + phoneNums);
 
         const addresses = $('.address').text();
         console.log(addresses);
-        // console.log("length " + addresses.length);
+        console.log("done");
     })
     .catch(function (err) {
         //handle error
