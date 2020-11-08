fccdb880832e4d96f23a381c7e9c4d23d6190a68
jvent <justine.ventura14@gmail.com>
2020-11-07 19:40:48 -0700

    better parsing of second page

--- page.js
+++ page.js
@@ -19,73 +19,87 @@ rp(url)
 
             const children = $(ul).children();
             children.each(async (i, li) => {
+                // console.log("sup");
                 let firstPageInfo = [];
-                const classes = ["specialty", "phone", "address", "miles"];
-                index = 0;
+                const classes = ["name", "specialty", "phone", "address", "miles"];
                 const children = $(li).children();
-                console.log("children " + children);
                 const link = $(li).find('h2').find('a').attr('href');
-                console.log("name " + $(li).find('h2').find('a').text());
                 const path = "https://findadentist.ada.org";
-                console.log("link : " + path + link);
-                for (var i = 0; i < classes.length; i++) {
-                    const info = children.find("." + classes[i]).text();
 
+                // console.log("link : " + path + link); // send to other function
+                // console.log("name " + $(li).find('h2').find('a').text());
+                firstPageInfo[0] = $(li).find('h2').find('a').text();
+
+                for (var i = 1; i < classes.length; i++) {
+                    const info = children.find("." + classes[i]).text();
                     firstPageInfo[i] = info;
                 }
                 for (var i = 0; i < firstPageInfo.length; i++) {
                     console.log(firstPageInfo[i]);
                 }
-
-
+                // let res = await visitDentistPage(link);
+                // console.log("res " + res);
 
             })
         })
-        // test link
-        writeToJSON(firstPageInfo);
-
+        // TODO: Get these other function calls async
         // visitDentistPage("https://findadentist.ada.org//ga/fulton/atlanta/general-practice/dr-bruce-ashendorf-1649092");
-
+        visitDentistPage("https://findadentist.ada.org/ga/dekalb/decatur/general-practice/dr-t-james-brown-3154033");
+        // writeToJSON(firstPageInfo);
     })
     .catch(function (err) {
-        //handle error
+        // handle error
     });
-// let firstPageInfo = ["Dentist", "890", "Road", "8.8"];
-// writeToJSON(firstPageInfo);
+
 function writeToJSON(values) {
     console.log("write to json");
     var obj = { info: [] };
-    obj.info.push({ 'specialty': values[0], 'phone': values[1], 'address': values[2], 'miles': values[3] });
+    obj.info.push({ 'name': values[0], 'specialty': values[1], 'phone': values[2], 'address': values[3], 'miles': values[4] });
     var json = JSON.stringify(obj);
     var fs = require('fs');
     fs.writeFile('output.json', json, 'utf8', callback);
 }
+
+const dentistInfo = []
 async function visitDentistPage(link) {
     const url = link;
     const res = [];
+    const unwantedDescriptors = ["Phone", "Email", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday", "Address",
+        "Specialities"];
+
     rp(url)
         .then(function (html) {
             const $ = cheerio.load(html);
             console.log("in dentist page");
-            // const website = $('.definition-list--last').get('a').attr('href');
-            // console.log("website " + website);
-            // console.log($('dt:contains("Gender"));
-            // $('.definition-list--last a').get().map(x => $(x).attr('href'))
-            $('.definition-list--last').each((i, dl) => {
-                const children = $(dl).children();
-                children.each((i, li) => {
-                    const children = $(li).children();
-                    children.each((i, dd) => {
-                        // console.log($(a).attr('href'));
-                        // console.log($(dd).text());
-                        res.push($(dd).text());
+            $('dt').each((index, element) => {
+                console.log($(element).text());
+                console.log($(element).next().text());
+                console.log(" _____________");
+                const key = $(element).text();
+                if (!unwantedDescriptors.includes(key)) {
+                    dentistInfo.push({ [$(element).text()]: $(element).next().text() });
+                }
+            });
+            console.log("here");
+            for (var i = 0; i < dentistInfo.length; i++) {
+                console.log("i " + i + "  " + dentistInfo[i]);
+            }
+            console.log("info" + JSON.stringify(dentistInfo));
+            // $('dd').each((index, element) => {
+            //     console.log($(element).text());
+            // });
+            // $('dl').each((i, dl) => {
+            //     console.log("fuck");
+            //     //var category = $('dt').innerHTML();
+            //     const children = $(dl).children();
+            //     console.log("dl " + children);
 
-                    })
-                })
-            })
-            return new Promise.all(res);
+            //     console.log("cat " + children.find('dt').text());
+            // })
+
             console.log("done");
         }).catch(function (err) {
             //handle error
         });
 }
+
